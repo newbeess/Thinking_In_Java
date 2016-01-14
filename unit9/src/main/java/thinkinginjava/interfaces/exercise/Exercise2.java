ec4376b128eeb69d7cb859a981a570cf639f28bb
newbeess <newbeessfu@gmail.com>
2016-01-14 21:18:26 +0800

    complete Exercise2

---
+++ unit9/src/main/java/thinkinginjava/interfaces/exercise/Exercise2.java
@@ -0,0 +1,13 @@
+package thinkinginjava.interfaces.exercise;
+
+/**
+ * Create an abstract class which contains no abstract method,
+ * vercify we cann't create any instances of it.
+ */
+abstract class AbstractClassWithNoAbstractMethod{
+}
+public class Exercise2 {
+	public static void main(String[] args){
+		new AbstractClassWithNoAbstractMethod();
+	}
+}
