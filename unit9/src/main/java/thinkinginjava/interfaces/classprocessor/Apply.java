443e1c48052f646ee4efbbaf38702d98e0bb27c5
newbeess <newbeessfu@gmail.com>
2016-01-20 22:50:54 +0800

    apply.java

---
+++ unit9/src/main/java/thinkinginjava/interfaces/classprocessor/Apply.java
@@ -0,0 +1,42 @@
+package thinkinginjava.interfaces.classprocessor;
+
+/**
+ *
+ */
+import java.util.*;
+import static com.elephant.util.Print.*;
+class Processor{
+	public String name(){
+		return getClass().getSimpleName();
+	}
+	Object process(Object input){return input;}
+}
+class Upcase extends Processor{
+	String process(Object input){
+		//Covariant return
+		return ((String)input).toUpperCase();
+	}
+}
+class Downcase extends Processor{
+	String process(Object input){
+		return ((String)input).toLowerCase();
+	}
+}
+class Splitter extends Processor{
+	String process(Object input){
+		//The split() argument divides a String into pieces
+		return Arrays.toString(((String)input).split(" "));
+	}
+}
+public class Apply {
+	public static void process(Processor p,Object s){
+		print("Using Processor "+p.name());
+		print(p.process(s));
+	}
+	public static String s="Disagreement with briefs is by defination incorrect ";
+	public static void main(String[] args){
+		process(new Upcase(),s);
+		process(new Downcase(),s);
+		process(new Splitter(),s);
+	}
+}
