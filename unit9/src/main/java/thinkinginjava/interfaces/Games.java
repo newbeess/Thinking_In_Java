2bd497683648e87befab3846474824152b312edc
newbeess <newbeessfu@gmail.com>
2016-01-23 14:41:48 +0800

    Game

---
+++ unit9/src/main/java/thinkinginjava/interfaces/Games.java
@@ -0,0 +1,50 @@
+package thinkinginjava.interfaces;
+import static com.elephant.util.Print.*;
+/**
+ * 	A game framework using Fantory Method
+ */
+
+interface Game{boolean move();}
+interface GameFactory{Game getGame();}
+
+/**
+ * Checkers and CheckersFactory
+ */
+class Checkers implements Game{
+	private int moves=0;
+	private static final int MOVES=3;
+	public boolean move(){
+		print("Checkers move "+ moves);
+		return ++moves!=MOVES;
+	}
+}
+class CheckersFactory implements GameFactory{
+	public Game getGame() {return new Checkers();}
+}
+
+/**
+ * 	Chess and ChessFactory
+ */
+class Chess implements Game{
+	private int moves=0;
+	private static final int MOVES=4;
+	public boolean move() {
+		print("Chess move "+moves);
+		return ++moves!=MOVES;
+	}
+}
+class ChessFactory implements GameFactory{
+	public Game getGame() {return new Chess();}
+}
+
+public class Games {
+	public static void playGame(GameFactory factory){
+		Game s=factory.getGame();
+		while (s.move())
+			;
+	}
+	public static void main(String[] args){
+		playGame(new CheckersFactory());
+		playGame(new ChessFactory());
+	}
+}
