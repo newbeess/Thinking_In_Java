0e2c559527127882439c4693f6b3ec32a11d893f
newbeess <newbeessfu@gmail.com>
2016-02-01 01:03:10 +0800

    complete exercise1

--- unit10/src/main/java/com/elephant/innerclasses/exercises/Exercise1.java
+++ unit10/src/main/java/com/elephant/innerclasses/exercises/Exercise1.java
@@ -3,5 +3,15 @@ package com.elephant.innerclasses.exercises;
 /**
  *
  */
+class Outer{
+	class Inner{}
+	public Inner returnInner(){return new Inner();}
+}
+
 public class Exercise1 {
+	public static void main(String[] args){
+		//Outer.Inner i=new Outer.Inner();
+		Outer o=new Outer();
+		Outer.Inner i=o.returnInner();
+	}
 }
