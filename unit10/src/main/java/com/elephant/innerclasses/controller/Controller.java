b3ef92cb8cdd3ec299c21a5f214b80ab56137001
newbeess <newbeessfu@gmail.com>
2016-02-08 23:44:01 +0800

    Event and Controller

---
+++ unit10/src/main/java/com/elephant/innerclasses/controller/Controller.java
@@ -0,0 +1,22 @@
+package com.elephant.innerclasses.controller;
+import java.util.*;
+
+/**
+ *	The useable framework for control systems
+ */
+
+public class Controller {
+	//	A class from java.util to hold Event objects:
+	private List<Event> eventList=new ArrayList<Event>();
+	public void addEvent(Event c){eventList.add(c);}
+	public void run(){
+		while (eventList.size()>0)
+			//	Make a copy so you're not modifying the list while you're selecting the elements in it
+		for (Event e : new ArrayList<Event>(eventList))
+			if (e.ready()){
+				System.out.println(e);
+				e.action();
+				eventList.remove(e);
+			}
+	}
+}
---
+++ unit10/src/main/java/com/elephant/innerclasses/controller/Event.java
@@ -0,0 +1,37 @@
+package com.elephant.innerclasses.controller;
+
+/**
+ * 	The common methods for any control event
+ */
+
+
+public abstract class Event {
+
+	private long eventTime;
+	protected final long delayTime;
+
+	public Event(long delayTime){
+		this.delayTime=delayTime;
+		start();
+	}
+
+	public void start(){
+		eventTime=System.nanoTime()*delayTime;
+	}
+
+	public boolean ready(){
+		return System.nanoTime()>=eventTime;
+	}
+
+	public abstract void action();
+
+	public static void main(String[] args){
+		long start=System.nanoTime();
+		long time1=System.currentTimeMillis();
+		System.out.println(time1/1000/60/60/24/365+1970);
+		System.out.println((System.nanoTime()-start)/1000000.0);
+
+
+	}
+
+}
