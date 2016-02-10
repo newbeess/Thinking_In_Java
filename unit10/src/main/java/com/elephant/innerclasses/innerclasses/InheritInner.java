4534356063ca0b5510aaf4ed91eeac2f3dc421d0
newbeess <newbeessfu@gmail.com>
2016-02-11 00:47:11 +0800

    complete InheritInner

---
+++ unit10/src/main/java/com/elephant/innerclasses/innerclasses/InheritInner.java
@@ -0,0 +1,20 @@
+package com.elephant.innerclasses.innerclasses;
+
+/**
+ *	Inheriting an inner class
+ */
+
+class WithInner{
+	class Inner{}
+}
+
+public class InheritInner extends WithInner.Inner{
+	//!	Inheritinner(){}
+	InheritInner(WithInner wi){
+		wi.super();
+	}
+	public static void main(String[] args){
+		WithInner wi=new WithInner();
+		InheritInner Ii=new InheritInner(wi);
+	}
+}
