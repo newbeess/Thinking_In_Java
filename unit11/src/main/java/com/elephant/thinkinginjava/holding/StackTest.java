b1c0ab3ca2e4474e639ef374024abff3e9886d95
newbeess <newbeessfu@gmail.com>
2016-02-23 16:08:23 +0800

    complete StackTest

---
+++ unit11/src/main/java/com/elephant/thinkinginjava/holding/StackTest.java
@@ -0,0 +1,13 @@
+package com.elephant.thinkinginjava.holding;
+
+import  com.elephant.util.*;
+
+public class StackTest {
+	public static void main(String[] args){
+		Stack<String> stack=new Stack<String>();
+		for (String s : "My dog has fleas".split(" "))
+			stack.push(s);
+		while (!stack.empty())
+			System.out.print(stack.pop()+" ");
+	}
+}
