70a46520d6675b7eab9b983c3f435cfb38afad67
newbeess <newbeessfu@gmail.com>
2016-02-24 19:03:39 +0800

    modify Statistics

--- unit11/src/main/java/com/elephant/thinkinginjava/holding/Statistics.java
+++ unit11/src/main/java/com/elephant/thinkinginjava/holding/Statistics.java
@@ -8,11 +8,16 @@ public class Statistics {
 	public static void main(String[] args){
 		Random random=new Random(47);
 		Map<Integer,Integer> map=new HashMap<Integer, Integer>();
+		System.out.println(map);
 		for (int i=0;i<10000;i++){
 			// Produce a number between 0 and 20
 			int r=random.nextInt(20);
 			Integer freq= map.get(r);
 			map.put(r,freq==null? 1: freq+1);
+			if (i==1)
+				System.out.println(map);
+			if (i==2)
+				System.out.println(map);
 		}
 		System.out.println(map);
 	}
