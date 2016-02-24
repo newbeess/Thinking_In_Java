958f406aa7c8dcacf992103184a54f2d98bc5e91
newbeess <newbeessfu@gmail.com>
2016-02-24 15:49:50 +0800

    complete Statistics

---
+++ unit11/src/main/java/com/elephant/thinkinginjava/holding/Statistics.java
@@ -0,0 +1,19 @@
+package com.elephant.thinkinginjava.holding;
+import java.util.*;
+/**
+ * Sample demonstration of HashMap
+ */
+
+public class Statistics {
+	public static void main(String[] args){
+		Random random=new Random(47);
+		Map<Integer,Integer> map=new HashMap<Integer, Integer>();
+		for (int i=0;i<10000;i++){
+			// Produce a number between 0 and 20
+			int r=random.nextInt(20);
+			Integer freq= map.get(r);
+			map.put(r,freq==null? 1: freq+1);
+		}
+		System.out.println(map);
+	}
+}
