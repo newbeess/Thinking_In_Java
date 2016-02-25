2e704de115722ae130309b04c259200e4538894c
newbeess <newbeessfu@gmail.com>
2016-02-25 14:38:51 +0800

    complete EnvironmentVariable

---
+++ unit11/src/main/java/com/elephant/thinkinginjava/holding/EnvironmentVariables.java
@@ -0,0 +1,10 @@
+package com.elephant.thinkinginjava.holding;
+import java.util.*;
+
+public class EnvironmentVariables {
+	public static void main(String[] args){
+		for (Map.Entry entry : System.getenv().entrySet()){
+			System.out.println(entry.getKey()+": "+entry.getValue());
+		}
+	}
+}
