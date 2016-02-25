f7c69b85a8383a1c5bc288e403b13c02ca9fef1e
newbeess <newbeessfu@gmail.com>
2016-02-25 10:05:23 +0800

    add comments to PriorityQueueDemo

--- unit11/src/main/java/com/elephant/thinkinginjava/holding/PriorityQueueDemo.java
+++ unit11/src/main/java/com/elephant/thinkinginjava/holding/PriorityQueueDemo.java
@@ -5,32 +5,40 @@ public class PriorityQueueDemo {
 	public static void main(String[] args){
 		PriorityQueue<Integer> priorityQueue=new PriorityQueue<Integer>();
 		Random random=new Random(47);
-
+		// Use offer to fill queue
 		for (int i=0;i<10;i++)
 			priorityQueue.offer(random.nextInt(i+10));
 		QueueDemo.printQ(priorityQueue);
 
+		// Use List to fill queue
 		List<Integer> ints=Arrays.asList(25,22,20,18,14,9,3,1,1,2,3,9,14,18,21,23,25);
 		priorityQueue=new PriorityQueue<Integer>(ints);
 		QueueDemo.printQ(priorityQueue);
 
+		// Use List to fill queue
+		// Order the elements according to the specified comparator
 		priorityQueue=new PriorityQueue<Integer>(
 			ints.size(),Collections.reverseOrder()
 		);
 		priorityQueue.addAll(ints);
 		QueueDemo.printQ(priorityQueue);
 
+		// Use List to fill queue
 		String fact="EDUCATION SHOULD ESCHEW OBFUSCATION";
-		List<String> strings=Arrays.asList(fact.split(" "));
+		List<String> strings=Arrays.asList(fact.split(""));
 		PriorityQueue<String> stringPQ=new PriorityQueue<String>(strings);
 		QueueDemo.printQ(stringPQ);
 
+		// Use List to fill queue
+		// Order the elements according to the specified comparator
 		stringPQ=new PriorityQueue<String>(
 			strings.size(),Collections.reverseOrder()
 		);
 		stringPQ.addAll(strings);
 		QueueDemo.printQ(stringPQ);
 
+		// Uset Set to fill queue
+		// no repeat element
 		Set<Character> charSet=new HashSet<Character>();
 		for (char c : fact.toCharArray())
 			charSet.add(c); //Autoboxing
