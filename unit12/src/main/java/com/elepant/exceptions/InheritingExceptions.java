3eed973b701a72cd329bb5cf7107d9445151edca
newbeess <newbeessfu@gmail.com>
2016-02-25 19:31:14 +0800

    complete inheritExceptions

---
+++ unit12/src/main/java/com/elepant/exceptions/InheritingExceptions.java
@@ -0,0 +1,23 @@
+package com.elepant.exceptions;
+
+/**
+ * Create your own exceptions
+ */
+
+class SimpleException extends Exception{}
+
+public class InheritingExceptions {
+	public void f() throws SimpleException{
+		System.out.println("Throw SimpleException from f()");
+		throw new SimpleException();
+	}
+	public static void main(String[] args){
+		InheritingExceptions sed=new InheritingExceptions();
+		try {
+			sed.f();
+		}
+		catch (SimpleException e){
+			System.out.println("Caught it!");
+		}
+	}
+}
