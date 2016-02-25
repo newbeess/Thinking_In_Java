44bef55e81e23e35eae512651140425ab89053a3
newbeess <newbeessfu@gmail.com>
2016-02-25 15:24:13 +0800

    complete AdapterMethodIdiom

---
+++ unit11/src/main/java/com/elephant/thinkinginjava/holding/AdapterMethodIdiom.java
@@ -0,0 +1,40 @@
+package com.elephant.thinkinginjava.holding;
+import java.util.*;
+/**
+ * The "Adapter Method" idiom allows you to use foreach with additional kinds of Iterables
+ */
+
+class ReversibleArrayList<T> extends ArrayList<T>{
+	public ReversibleArrayList(Collection<T> c){super(c);}
+	public Iterable<T> reversed(){
+		return new Iterable<T>() {
+			public Iterator<T> iterator() {
+				return new Iterator<T>() {
+					int current=size()-1;
+					public boolean hasNext() {
+						return current>-1;
+					}
+					public T next() {
+						return get(current--);
+					}
+					public void remove(){
+						throw new UnsupportedOperationException();
+					}
+				};
+			}
+		};
+	}
+}
+public class AdapterMethodIdiom {
+	public static void main(String[] args){
+		ReversibleArrayList<String> ral=new ReversibleArrayList<String>(Arrays.asList("To be or not to be".split(" ")));
+		// Grabs the ordinary iterator via iterator()
+		for (String s : ral)
+			System.out.print(s+" ");
+		System.out.println();
+
+		// Hand it the Iterable of your choice
+		for (String s : ral.reversed())
+			System.out.print(s+" ");
+	}
+}
