8a18914481924cb0273ddb684bdbbde7e5b6f588
newbeess <newbeessfu@gmail.com>
2016-02-28 10:40:58 +0800

    complete LoggingException

--- unit12/src/main/java/com/elephant/exceptions/LoggingExceptions.java
+++ unit12/src/main/java/com/elephant/exceptions/LoggingExceptions.java
@@ -1,11 +1,31 @@
 package com.elephant.exceptions;
 import java.util.logging.*;
 import java.io.*;
+
 /**
  * An exception that reports through a Logger
  *
  */
+class LoggingException extends Exception{
+	private static Logger logger=Logger.getLogger("LoggingException");
+	public LoggingException(){
+		StringWriter trace=new StringWriter();
+		printStackTrace(new PrintWriter(trace));
+		logger.severe(trace.toString());
+	}
+}
 
 public class LoggingExceptions {
-
+	public static void main(String[] args){
+		try {
+			throw new LoggingException();
+		} catch (LoggingException e){
+			System.err.println("Caught "+e);
+		}
+		try {
+			throw new LoggingException();
+		} catch (LoggingException e){
+			System.err.println("Caught "+e);
+		}
+	}
 }
