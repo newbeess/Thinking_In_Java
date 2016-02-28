48375c82d25a3d9e692c8a773087c694dcdd8200
newbeess <newbeessfu@gmail.com>
2016-02-28 11:23:26 +0800

    complete LoggingExceptions2

---
+++ unit12/src/main/java/com/elephant/exceptions/LoggingExceptions2.java
@@ -0,0 +1,19 @@
+package com.elephant.exceptions;
+import java.util.logging.*;
+import java.io.*;
+
+public class LoggingExceptions2 {
+	private static Logger logger=Logger.getLogger("LoggingExceptions2");
+	static void logException(Exception e){
+		StringWriter trace=new StringWriter();
+		e.printStackTrace(new PrintWriter(trace));
+		logger.severe(trace.toString());
+	}
+	public static void main(String[] args){
+		try {
+			throw new NullPointerException();
+		} catch (NullPointerException e){
+			logException(e);
+		}
+	}
+}
