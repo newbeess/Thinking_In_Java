ada92fdf5f3ab2fef5210d7ebb9322c9d7e02236
newbeess <newbeessfu@gmail.com>
2016-03-01 21:18:50 +0800

    multipleReturns

---
+++ unit12/src/main/java/com/elephant/exceptions/MultipleReturns.java
@@ -0,0 +1,24 @@
+package com.elephant.exceptions;
+import static com.elephant.util.Print.*;
+
+public class MultipleReturns {
+	public static void f(int i){
+		print("Initialization that require cleanup");
+		try {
+			print("Point 1");
+			if (i == 1)return;
+			print("Point 2");
+			if (i == 2) return;
+			print("Point 3");
+			if (i == 3) return;
+			print("End");
+			return;
+		} finally {
+			print("Performing cleanup");
+		}
+	}
+	public static void main(String[] args){
+		for (int i=1;i<=4;i++)
+			f(i);
+	}
+}
