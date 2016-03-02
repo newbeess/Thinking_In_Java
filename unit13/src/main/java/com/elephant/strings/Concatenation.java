020552a54f10f936c6d543c5e55afd5ff60574da
newbeess <newbeessfu@gmail.com>
2016-03-02 21:37:52 +0800

    complete Concatenation

---
+++ unit13/src/main/java/com/elephant/strings/Concatenation.java
@@ -0,0 +1,10 @@
+package com.elephant.strings;
+
+public class Concatenation {
+	public static void main(String[] args){
+		String s1="hello";
+		String s2="man";
+		String s="abc"+s1+", "+s2;
+		System.out.println(s);
+	}
+}
