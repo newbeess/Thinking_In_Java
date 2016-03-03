67c5a65ffb635b7c1bca3cdb9449eeddf4ea0b35
newbeess <newbeessfu@gmail.com>
2016-03-03 20:47:40 +0800

    complete SimpleFormat

---
+++ unit13/src/main/java/com/elephant/strings/SimpleFormat.java
@@ -0,0 +1,15 @@
+package com.elephant.strings;
+
+
+public class SimpleFormat {
+	public static void main(String[] args){
+		int x=5;
+		double y=5.332542;
+		// The old way
+		System.out.println("Row 1: ["+x+" "+y+"]");
+		// The new way
+		System.out.format("Row 1: [%d %f]\n",x,y);
+		// Or
+		System.out.printf("Row 1: [%d %f]\n",x,y);
+	}
+}
