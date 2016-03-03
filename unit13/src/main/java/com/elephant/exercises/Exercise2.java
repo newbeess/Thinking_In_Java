5941fab232be6609e9274a59f8fe237e5c2f988e
newbeess <newbeessfu@gmail.com>
2016-03-03 22:21:16 +0800

    complete Exercise4

--- unit13/src/main/java/com/elephant/exercises/Exercise2.java
+++ unit13/src/main/java/com/elephant/exercises/Exercise2.java
@@ -1,7 +1,15 @@
 package com.elephant.exercises;
 
-public class Exercise1 {
-	public static void main(String[] args){
+import java.util.*;
 
+public class Exercise2 {
+	public String toString(){
+		return "Exercise"+super.toString()+"\n";
+	}
+	public static void main(String[] args){
+		List<Exercise2> list=new ArrayList<Exercise2>();
+		for (int i=0;i<10;i++)
+			list.add(new Exercise2());
+		System.out.println(list);
 	}
 }
--- unit13/src/main/java/com/elephant/exercises/Exercise3.java
+++ unit13/src/main/java/com/elephant/exercises/Exercise3.java
@@ -1,7 +1,27 @@
 package com.elephant.exercises;
+import java.io.*;
+import java.util.*;
 
-/**
- * Created by elephant on 16/3/3.
- */
+class Turtle {
+	private String name;
+	private Formatter f;
+	public Turtle(String name,Formatter f){
+		this.name=name;
+		this.f=f;
+	}
+	public void move(int x,int y){
+		f.format("%s The Turtle is at (%d,%d)\n",name,x,y);
+	}
+}
 public class Exercise3 {
+	public static void main(String[] args){
+		Turtle tommy=new Turtle("Tommy",new Formatter(System.err));
+		Turtle terry=new Turtle("Terry",new Formatter(System.err));
+		tommy.move(0,0);
+		terry.move(4,8);
+		tommy.move(3,4);
+		terry.move(2,5);
+		tommy.move(3,3);
+		terry.move(3,3);
+	}
 }
