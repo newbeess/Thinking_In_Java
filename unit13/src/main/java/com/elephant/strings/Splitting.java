6adb5b4ac094ec1d8c46f15807c81726f026ff2a
newbeess <newbeessfu@gmail.com>
2016-03-04 16:19:33 +0800

    complete Splitting

---
+++ unit13/src/main/java/com/elephant/strings/Splitting.java
@@ -0,0 +1,15 @@
+package com.elephant.strings;
+import java.util.*;
+
+public class Splitting {
+	public static String knights=
+			"Then,when you have found the shrubeery,you must cut down the mightiest tree in the forest... with ... a herring!";
+	public static void split(String regex){
+		System.out.println(Arrays.toString(knights.split(regex)));
+	}
+	public static void main(String[] args){
+		split(" ");
+		split("\\W+");
+		split("n\\W+");
+	}
+}
