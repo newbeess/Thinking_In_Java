8131bd9418010675395dcadfafb46eb18de3ebcc
newbeess <newbeessfu@gmail.com>
2016-03-05 10:45:40 +0800

    StartEnd

---
+++ unit13/src/main/java/com/elephant/strings/StartEnd.java
@@ -0,0 +1,42 @@
+package com.elephant.strings;
+import java.util.regex.*;
+import static com.elephant.util.Print.*;
+
+public class StartEnd {
+	public static String input=
+			"As long as there is injustice, whenever a\n"+
+			"Targathian baby cries out, wherever a distress\n"+
+			"signal sounds among the stars ... We'll be there.\n" +
+			"This fine ship, and this fine crew ...\n"+
+			"Never give up! Never surrender!";
+	private static class Display{
+		private boolean regexPrinted=false;
+		private String regex;
+		Display(String regex){this.regex=regex;}
+		void display(String message){
+			if (!regexPrinted){
+				print(regex);
+				regexPrinted=true;
+			}
+			print(message);
+		}
+	}
+	static void examine(String s, String regex){
+		Display d =new Display(regex);
+		Pattern p=Pattern.compile(regex);
+		Matcher m=p.matcher(s);
+		while (m.find())
+			d.display("find() '"+m.group()+"'start= "+m.start()+" end= "+m.end());
+		if (m.lookingAt())
+			d.display("lookingAt() start= "+m.start()+" end= "+m.end());
+		if (m.matches())
+			d.display("matches() start= "+m.start()+" end= "+m.end());
+	}
+	public static void main(String[] args){
+		for (String in : input.split("\n")) {
+			print("input : " + in);
+			for (String regex : new String[]{"\\w*ere\\w*", "\\w*ever", "T\\w+", "Never.*?!"})
+				examine(in, regex);
+		}
+	}
+}
