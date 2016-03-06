571b00d918f15f7567a40aad673178eba0532cb1
newbeess <newbeessfu@gmail.com>
2016-03-06 12:41:12 +0800

    complete ThreatAnalyzer

---
+++ unit13/src/main/java/com/elephant/strings/ThreatAnalyzer.java
@@ -0,0 +1,24 @@
+package com.elephant.strings;
+import java.util.regex.*;
+import java.util.*;
+
+public class ThreatAnalyzer {
+	static String threatData=
+		"58.27.82.161@02/10/2005\n"+
+		"204.45.234.40@02/11/2005\n"+
+		"58.27.82.161@02/11/2005\n"+
+		"58.27.82.161@02/12/2005\n"+
+		"58.27.82.161@02/12/2005\n"+
+		"[Next log section with different data format]";
+	public static void main(String[] args){
+		Scanner scanner=new Scanner(threatData);
+		String pattern="(\\d+[.]\\d+[.]\\d+[.]\\d+)@(\\d{2}/\\d{2}/\\d{4})";
+		while (scanner.hasNext(pattern)){
+			scanner.next(pattern);
+			MatchResult matchResult=scanner.match();
+			String ip=matchResult.group(1);
+			String date=matchResult.group(2);
+			System.out.format("Threat on %s from %s\n",date,ip);
+		}
+	}
+}
