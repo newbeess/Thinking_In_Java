e06359ccfa9954053459dc3bf9bc92078e2177e3
newbeess <newbeessfu@gmail.com>
2016-03-06 09:48:25 +0800

    complete JGrep

---
+++ unit13/src/main/java/com/elephant/strings/JGrep.java
@@ -0,0 +1,25 @@
+package com.elephant.strings;
+import java.util.regex.*;
+import com.elephant.util.*;
+
+// A very simple version of the "grep" program.
+// {Args: JGrep.java "\\b[Ssct]\\w+"}
+
+
+public class JGrep {
+	public static void main(String[] args) throws Exception{
+		if (args.length<2){
+			System.out.println("Usage: java JGrep file regex");
+			System.exit(0);
+		}
+		Pattern p=Pattern.compile(args[1]);
+		// Iterate through the lines of the input file
+		int index=0;
+		Matcher m=p.matcher("");
+		for (String line : new TextFile(args[0])){
+			m.reset(line);
+			while (m.find())
+				System.out.println(index++ +": "+m.group()+": "+m.start());
+		}
+	}
+}
