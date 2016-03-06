bd6f33522f8e8b195874a7f10543ee356c170f89
newbeess <newbeessfu@gmail.com>
2016-03-06 12:29:59 +0800

    complete ScannerDelimiter

---
+++ unit13/src/main/java/com/elephant/strings/ScannerDelimiter.java
@@ -0,0 +1,11 @@
+package com.elephant.strings;
+import java.util.*;
+
+public class ScannerDelimiter {
+	public static void main(String[] args){
+		Scanner scanner=new Scanner("12 ,       42 ,78,99,42");
+		scanner.useDelimiter("\\s*,\\s*");
+		while (scanner.hasNext())
+			System.out.println(scanner.nextInt());
+	}
+}
