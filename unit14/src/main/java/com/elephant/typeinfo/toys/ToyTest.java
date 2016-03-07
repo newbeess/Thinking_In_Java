88bb6f25708345723f6d3e21b9c7bd688b6e9754
newbeess <newbeessfu@gmail.com>
2016-03-07 22:07:28 +0800

    complete ToyTest

---
+++ unit14/src/main/java/com/elephant/typeinfo/toys/ToyTest.java
@@ -0,0 +1,51 @@
+package com.elephant.typeinfo.toys;
+import static com.elephant.util.Print.*;
+/**
+ * Testing class Class
+ */
+
+interface HasBatteries{}
+interface Waterproof{}
+interface Shoots{}
+
+class Toy{
+	Toy(){}
+	Toy(int i){}
+}
+class FancyToy extends Toy implements HasBatteries,Waterproof,Shoots{
+	FancyToy(){super(1);}
+}
+
+public class ToyTest {
+	static void printInfo(Class cc){
+		print("Class name: "+cc.getName()+" is interface? ["+cc.isInterface()+"]");
+		print("Simple name: "+cc.getSimpleName());
+		print("Canonical name : "+cc.getCanonicalName());
+	}
+
+	public static void main(String[] args){
+		Class c=null;
+		try {
+			c=Class.forName("com.elephant.typeinfo.toys.FancyToy");
+		}catch (ClassNotFoundException e){
+			print("Can't find FancyToy");
+			System.exit(1);
+		}
+		printInfo(c);
+		for (Class face : c.getInterfaces())
+			printInfo(face);
+		Class up=c.getSuperclass();
+		Object obj=null;
+		try {
+			// Require default constructor
+			obj=up.newInstance();
+		} catch (InstantiationException e){
+			print("Can't instantiate");
+			System.exit(1);
+		} catch (IllegalAccessException e){
+			print("Can't access");
+			System.exit(1);
+		}
+		printInfo(obj.getClass());
+	}
+}
