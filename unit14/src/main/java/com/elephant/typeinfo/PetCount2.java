5e0eadfc0652f388b082ea2961a4da4f34b5365c
newbeess <newbeessfu@gmail.com>
2016-03-13 21:34:25 +0800

    PetCount2

---
+++ unit14/src/main/java/com/elephant/typeinfo/PetCount2.java
@@ -0,0 +1,8 @@
+package com.elephant.typeinfo;
+import com.elephant.typeinfo.pets.*;
+
+public class PetCount2 {
+	public static void main(String[] args){
+		PetCount.countPets(Pets.creator);
+	}
+}
