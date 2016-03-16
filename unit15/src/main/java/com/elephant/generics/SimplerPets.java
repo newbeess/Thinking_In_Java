c70d8169a6e2e07330ce2932a4c8ad720f2c7513
newbeess <newbeessfu@gmail.com>
2016-03-16 21:21:39 +0800

    SimplerPets

---
+++ unit15/src/main/java/com/elephant/generics/SimplerPets.java
@@ -0,0 +1,11 @@
+package com.elephant.generics;
+import typeinfo.pets.*;
+import java.util.*;
+import com.elephant.util.*;
+
+public class SimplerPets {
+	public static void main(String[] args){
+		Map<Person,List<? extends Pet>> peoplePet=New.map();
+
+	}
+}
