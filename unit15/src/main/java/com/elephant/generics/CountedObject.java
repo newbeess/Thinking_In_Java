7b66deabb87e76d975265bdbe44e7d82a78cddc9
newbeess <newbeessfu@gmail.com>
2016-04-03 13:54:49 +0800

    add simple class with default constructor

---
+++ unit15/src/main/java/com/elephant/generics/CountedObject.java
@@ -0,0 +1,11 @@
+package com.elephant.generics;
+
+/**
+ * Created by elephant on 16/4/3.
+ */
+public class CountedObject {
+	private static long counter=0;
+	private final long id=counter++;
+	public long id(){return id;}
+	public String toString(){return "CountedObject "+id;}
+}
