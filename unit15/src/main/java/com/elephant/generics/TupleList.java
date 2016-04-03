116de557293e47fb4138de5898bb23d4452fde84
newbeess <newbeessfu@gmail.com>
2016-04-03 16:32:07 +0800

    TupleList

---
+++ unit15/src/main/java/com/elephant/generics/TupleList.java
@@ -0,0 +1,15 @@
+package com.elephant.generics;
+import java.util.*;
+import com.elephant.util.*;
+/**
+ * Combining generic types to make complex generic types.
+ */
+public class TupleList<A,B,C,D> extends ArrayList<FourTuple<A,B,C,D>> {
+	public static void main(String[] args){
+		TupleList<Vehicle,Amphibian,String,Integer> tl=new TupleList<Vehicle, Amphibian, String, Integer>();
+		tl.add(TupleTest.h());
+		tl.add(TupleTest.h());
+		for (FourTuple<Vehicle,Amphibian,String,Integer> i : tl)
+			System.out.println(i);
+	}
+}
