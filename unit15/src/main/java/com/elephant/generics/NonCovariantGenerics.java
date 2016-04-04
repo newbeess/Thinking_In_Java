375f3fa306b839bcac3bb8c574340217a45d3a2f
newbeess <newbeessfu@gmail.com>
2016-04-04 21:55:08 +0800

    NonCovariantGenerics

---
+++ unit15/src/main/java/com/elephant/generics/NonCovariantGenerics.java
@@ -0,0 +1,12 @@
+package com.elephant.generics;
+import java.util.*;
+/**
+ *  Fruit类型的容器装载的是Fruit和子类
+ *  Apple类型的容器装载的是Apple和子类
+ */
+public class NonCovariantGenerics {
+	public static void main(String[] args){
+		//List<Fruit> list=new ArrayList<Apple>();
+		//
+	}
+}
