c38be3069f5c674fffb8671335f3d10ee9d7a2a3
newbeess <newbeessfu@gmail.com>
2016-04-06 20:10:37 +0800

    multidimentionalPrimitiveArray

---
+++ unit16/src/main/java/com/elephant/arrays/MultidimensionalPrimitiveArray.java
@@ -0,0 +1,14 @@
+package com.elephant.arrays;
+import java.util.*;
+/**
+ * Creating multidimensional arrays
+ */
+public class MultidimensionalPrimitiveArray {
+	public static void main(String[] args){
+		int[][] a={
+				{1,2,3},
+				{4,5,6}
+		};
+		System.out.println(Arrays.deepToString(a));
+	}
+}
