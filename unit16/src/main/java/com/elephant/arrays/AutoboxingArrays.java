1bffac568007b452b88125665edd2106895c12f9
newbeess <newbeessfu@gmail.com>
2016-04-06 21:06:22 +0800

    Autoboxing

---
+++ unit16/src/main/java/com/elephant/arrays/AutoboxingArrays.java
@@ -0,0 +1,17 @@
+package com.elephant.arrays;
+import java.util.*;
+/**
+ * Created by elephant on 16/4/6.
+ */
+public class AutoboxingArrays {
+	public static void main(String[] args){
+		Integer[][] a={
+				{1,2,3,4,5,6,7,8,9,0},
+				{12,21,22,34},
+				{51,52,45},
+				{15,16,17,67,88,99}
+		};
+		System.out.println(Arrays.deepToString(a));
+	}
+
+}
