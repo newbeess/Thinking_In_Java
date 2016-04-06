b98a9aec8e0a973b237c2e12ad81d14e2ab0fd2a
newbeess <newbeessfu@gmail.com>
2016-04-06 20:19:14 +0800

    3-D array

---
+++ unit16/src/main/java/com/elephant/arrays/ThreeDWithNew.java
@@ -0,0 +1,11 @@
+package com.elephant.arrays;
+import java.util.*;
+/**
+ * Created by elephant on 16/4/6.
+ */
+public class ThreeDWithNew {
+	public static void main(String[] args){
+		int[][][] a=new int[2][2][4];
+		System.out.println(Arrays.deepToString(a));
+	}
+}
