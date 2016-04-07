65c7b48a8ac2dd860af37c6da236aa3408f818a2
newbeess <newbeessfu@gmail.com>
2016-04-07 21:43:56 +0800

    Comptype

--- unit16/src/main/java/com/elephant/arrays/CompType.java
+++ unit16/src/main/java/com/elephant/arrays/CompType.java
@@ -31,7 +31,11 @@ public class CompType implements Comparable<CompType> {
 	}
 
 	public static void main(String[] args){
-		CompType[] a= Generated.array(new CompType[12],generator());
+		CompType[] a= {
+				new CompType(58,55),new CompType(93,61),new CompType(61,29),
+				new CompType(68,0),new CompType(22,7),new CompType(48,12),
+				new CompType(1,88),new CompType(41,80),new CompType(11,90)
+		};
 		print("before sorting: ");
 		print(Arrays.toString(a));
 		Arrays.sort(a);
