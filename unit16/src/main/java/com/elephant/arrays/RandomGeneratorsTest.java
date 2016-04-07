950ebdc471bcf97ea885565d6e665a083a2f02c3
newbeess <newbeessfu@gmail.com>
2016-04-07 19:46:16 +0800

    Random test

---
+++ unit16/src/main/java/com/elephant/arrays/RandomGeneratorsTest.java
@@ -0,0 +1,11 @@
+package com.elephant.arrays;
+import com.elephant.util.RandomGenerator;
+/**
+ * Created by elephant on 16/4/7.
+ */
+public class RandomGeneratorsTest {
+	public static void main(String[] args){
+		GeneratorTest.test(RandomGenerator.class);
+	}
+
+}
