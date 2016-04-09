e4f2f4f04dddc6c6f153d1a8967aa7e3204d634f
newbeess <newbeessfu@gmail.com>
2016-04-09 13:26:05 +0800

    Dir List 3

---
+++ unit18/src/main/java/com/elephant/io/DirList3.java
@@ -0,0 +1,28 @@
+package com.elephant.io;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+/**
+ * Building the anonymous inner class "in-place"
+ */
+public class DirList3 {
+	public static void main(final String[] args){
+		File path=new File(".");
+		String[] list;
+		if (args.length == 0)
+			list=path.list();
+		else
+			list=path.list(new FilenameFilter() {
+				private Pattern pattern=Pattern.compile(args[0]);
+				public boolean accept(File dir, String name) {
+					return pattern.matcher(name).matches();
+				}
+			});
+		Arrays.sort(list,String.CASE_INSENSITIVE_ORDER);
+		for (String dirItem : list)
+			System.out.println(dirItem);
+
+	}
+}
