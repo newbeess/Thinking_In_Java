3a22a11a6166abe74f892d797d4c773aa3dd7ba2
newbeess <newbeessfu@gmail.com>
2016-04-10 17:23:17 +0800

    RandomAccessFile

--- unit18/src/main/java/com/elephant/io/FileOutputShortCut.java
+++ unit18/src/main/java/com/elephant/io/FileOutputShortCut.java
@@ -1,7 +1,22 @@
 package com.elephant.io;
-
+import java.io.*;
 /**
  * Created by elephant on 16/4/10.
  */
 public class FileOutputShortCut {
+	static  String file="/Users/elephant/dev/github_codes/Thinking_In_Java/unit18/BasicFileOutput1";
+	public static void main(String[] args) throws IOException{
+		BufferedReader in=new BufferedReader(
+				new StringReader(
+						BufferedInputFile.read("/Users/elephant/dev/github_codes/Thinking_In_Java/unit18/src/main/java/com/elephant/io/FileOutputShortCut.java")));
+// Here is the shortcut
+		PrintWriter out=new PrintWriter(file);
+		int lineCount=1;
+		String s;
+		while ((s=in.readLine())!=null)
+			out.println(lineCount++ +": "+s);
+		out.close();
+// show stored file
+		System.out.println(BufferedInputFile.read(file));
+	}
 }
--- unit18/src/main/java/com/elephant/io/StoringAndRecoveringData.java
+++ unit18/src/main/java/com/elephant/io/StoringAndRecoveringData.java
@@ -1,7 +1,27 @@
 package com.elephant.io;
-
+import java.io.*;
 /**
  * Created by elephant on 16/4/10.
  */
 public class StoringAndRecoveringData {
+	public static void main(String[] args) throws IOException{
+		DataOutputStream out=new DataOutputStream(
+				new BufferedOutputStream(
+						new FileOutputStream("Data.txt")));
+		out.writeDouble(3.1415926);
+		out.writeUTF("This is PI");
+		out.writeDouble(1.41413);
+		out.writeUTF("Square root of 2");
+		out.close();
+
+		DataInputStream in=new DataInputStream(
+			new BufferedInputStream(
+					new FileInputStream("Data.txt")));
+		System.out.println(in.readDouble());
+	// Only readUTF() will recover the Java-UTF String property
+		System.out.println(in.readUTF());
+		System.out.println(in.readDouble());
+		System.out.println(in.readUTF());
+
+	}
 }
