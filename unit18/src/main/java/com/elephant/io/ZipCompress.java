e939bc91b6239fc442681823ab031a6283dbb499
newbeess <newbeessfu@gmail.com>
2016-04-12 21:09:06 +0800

    compelte ZipCompress

---
+++ unit18/src/main/java/com/elephant/io/ZipCompress.java
@@ -0,0 +1,55 @@
+package com.elephant.io;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+import static com.elephant.util.Print.*;
+/**
+ * Use Zip compress to compress any number of files given on the command line.
+ */
+public class ZipCompress {
+	public static void main(String[] args) throws IOException{
+		FileOutputStream f=new FileOutputStream("test.zip");
+		CheckedOutputStream csum=new CheckedOutputStream(f,new Adler32());
+		ZipOutputStream zos=new ZipOutputStream(csum);
+		BufferedOutputStream out=new BufferedOutputStream(zos);
+		zos.setComment("A test of Java Zipping");
+		// No corresponding getComment(),though
+		for (String arg : args){
+			print("Writting file"+arg);
+			BufferedReader in=new BufferedReader(new FileReader(arg));
+			zos.putNextEntry(new ZipEntry(arg));
+			int c;
+			while ((c=in.read()) != -1)
+				out.write(c);
+			in.close();
+			out.close();
+		}
+// CehckSum valid only after the file has been closed!
+		print("CheckSum: "+csum.getChecksum().getValue());
+// Now extract the file
+		print("Reading file");
+		FileInputStream fi=new FileInputStream("test.zip");
+		CheckedInputStream csumi=new CheckedInputStream(fi,new Adler32());
+		ZipInputStream in2=new ZipInputStream(csumi);
+		BufferedInputStream bis=new BufferedInputStream(in2);
+		ZipEntry ze;
+		while ((ze = in2.getNextEntry()) != null){
+			print("Reading file "+ze);
+			int x;
+			while ((x=bis.read()) != -1)
+				System.out.write(x);
+		}
+		if (args.length ==1 )
+			print("CheckSum: "+csumi.getChecksum().getValue());
+		bis.close();
+// Alternative way to open and read zip files
+		ZipFile zf=new ZipFile("test.zip");
+		Enumeration e=zf.entries();
+		while (e.hasMoreElements()){
+			ZipEntry ze2=(ZipEntry)e.nextElement();
+			print("File: "+ze2);
+		}
+
+	}
+}
